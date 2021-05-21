d88dc78d5ad6ac2d3606b415b1e1243b0bc3bab4
JaviCeRodriguez <javicerodriguez@gmail.com>
2021-05-21 00:04:42 -0300

    MEdia clase hecha

---
+++ Clases/Clase_04/ejercicio1.js
@@ -0,0 +1,11 @@
+/*
+Hacer una función que reciba un numero y que retorne verdadero si es par o falso en caso
+contrario
+*/
+
+function parcito (value){
+    return value % 2 == 0 ? true : false
+}
+
+console.log(parcito(23))
+console.log(parcito(2))
---
+++ Clases/Clase_04/ejercicio2.js
@@ -0,0 +1,34 @@
+/*
+Hacer una función que reciba una serie de palabras separadas por espacios y que imprima
+la misma frase pero en orden inverso.
+*/
+
+function invertirFrase(n) {
+    let frase = "";
+    for (let i = n.length - 1; i >= 0; i--) {
+    frase += n[i];
+    }
+    return frase;
+   }
+
+const partialReverse = value => {
+    let aux_word = ''
+    let new_value = ''
+    const largo = value.length
+    for (let i = 0; i < largo; i++){
+        aux_word += value[i]
+        if (value[i] === ' ' || i == largo - 1){
+            new_value = aux_word + ' ' + new_value
+            aux_word = '' 
+        }
+    }
+    return new_value
+}
+
+frase = 'Hola como andas?'
+console.log(invertirFrase(frase))
+console.log(partialReverse(frase)) // andas? como Hola
+
+// Hola como andas
+// sadna omoc aloH
+// andas como Hola
